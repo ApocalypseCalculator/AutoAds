55f76e72cfcb29d0ffb9d16ef7ba63a70cd452fc
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-03-08 09:33:00 -0500

    selector

--- selector.bat
+++ selector.bat
@@ -1,5 +1,5 @@
 @echo off
-Title The absolute ultimate Browse for file dialog (w95 to w8) with start folder and filters
+Title File Browse Dialog
 (
     echo Function GetFileDlgEx(sIniDir,sFilter,sTitle^) 
     echo Set oDlg = CreateObject("WScript.Shell"^).Exec("mshta.exe ""about:<object id=d classid=clsid:3050f4e1-98b5-11cf-bb82-00aa00bdce0b></object><script>moveTo(0,-9999);eval(new ActiveXObject('Scripting.FileSystemObject').GetStandardStream(0).Read("^&Len(sIniDir^)^+Len(sFilter^)^+Len(sTitle^)+41^&"));function window.onload(){var p=/[^\0]*/;new ActiveXObject('Scripting.FileSystemObject').GetStandardStream(1).Write(p.exec(d.object.openfiledlg(iniDir,null,filter,title)));close();}</script><hta:application showintaskbar=no />"""^) 
