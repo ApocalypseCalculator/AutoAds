997b0d4fea8a2656a73c9282b14455dccf26f5df
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-03-07 21:39:55 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-const puppeteer = require('puppeteer-core');
+const readline = require('readline');
+const fs = require('fs');
+const chalk = require('chalk');
+const { spawn } = require('child_process');
+
+if (!fs.existsSync('./data.json')) {
+    console.log(chalk.red('Missing data.json file'));
+    process.exit(0);
+}
+
+let raw = fs.readFileSync('./data.json');
+let parsed = JSON.parse(raw);
+
+let rl = readline.createInterface({ input: process.stdin });
+
+if (fs.existsSync(parsed.chrome)) {
+    next();
+}
+else {
+    console.log(chalk.yellow('I did not find a Chrome/Chromium executable, please choose one: '));
+    const select = spawn('cmd.exe', ['/c', 'selector.bat']);
+    select.stdout.on('data', (data) => {
+        let path = data.toString();
+        if (/:\\/.test(path)) {
+            parsed.chrome = path.replace('\n', '').replace('\r', '').replace(/\\/g, '/');
+            let newraw = JSON.stringify(parsed);
+            fs.writeFileSync('./data.json', newraw);
+            next();
+        }
+        else {
+            console.log(chalk.red('Operation cancelled.'));
+            process.exit(0);
+        }
+    })
+}
+
+async function next() {
+    console.log(chalk.green('\nLogin time!\n'))
+    console.log(`Enter your email: `);
+    rl.question('', (email) => { //i have no fucking clue why rl.question doesnt print properly
+        if(/[0-9a-zA-Z]{1,100}@[0-9a-zA-Z]{1,100}/.test(email)){
+            nextnext(email);
+        }
+        else{
+            console.log(chalk.yellow('Invalid email'));
+            next();
+        }
+    })
+}
+
+async function nextnext(email) {
+    console.log('\nEnter your password: ');
+    rl.question('', (password) => {
+        if(password.length > 0 && password !== ''){
+            require('./simulate').d(email, password);
+        }
+        else{
+            console.log(chalk.yellow('Invalid password'));
+            nextnext(email);
+        }
+    })
+}
---
+++ simulate.js
@@ -0,0 +1,73 @@
+const puppeteer = require('puppeteer-core');
+const chalk = require('chalk');
+const fs = require('fs');
+
+const HUMAN = "https://politicsandwar.com/human";
+const ADLENGTH = 1 * 60 * 1000; //60 seconds
+
+async function d(email, pass) {
+    console.log();
+    let raw = fs.readFileSync('./data.json');
+    let parsed = JSON.parse(raw);
+    const browser = await puppeteer.launch({
+        executablePath: `${parsed.chrome}`,
+        args: [],
+        headless: true,
+        devtools: true,
+        ignoreDefaultArgs: ['--disable-extensions']
+    }).catch(err => {
+        console.log(chalk.red('Unable to launch your chromium executable'));
+        parsed.chrome = '';
+        let newraw = JSON.stringify(parsed);
+        fs.writeFileSync('./data.json', newraw);
+        console.log(err);
+        process.exit(0);
+    })
+    console.log(chalk.green('Logging in...'));
+    const page = await browser.newPage();
+    await (await page).goto("https://politicsandwar.com/login", { waitUntil: 'load' });
+    await (await page).type("input[name='email']", `${email}`);
+    await (await page).type("input[name='password']", `${pass}`);
+    await (await page).click("input[name='loginform']");
+    await (await page).waitForNavigation();
+    console.log(chalk.green('Logged in successfully...accessing rewarded ads page now'));
+    await (await page).goto("https://politicsandwar.com/rewarded-ads/", { waitUntil: "load" });
+    if (await (await (await page).content()).toLowerCase().includes('fail')) {
+        console.log(chalk.red('Login failed'));
+        process.exit(0);
+    }
+    else {
+        console.log(chalk.green('Loaded rewarded ads page...'));
+        if ((await page).url().includes(HUMAN)) {
+            console.log(chalk.yellow(`Human verification encountered. Please verify at ${HUMAN}/`));
+            process.exit(0);
+        }
+        else {
+            function click() {
+                return new Promise(resolve => {
+                    setTimeout(async function () {
+                        await (await page).click("#consent_agree_btn");
+                        console.log(chalk.green('Clicked stupid cookie button thingy'));
+                    }, 1000);
+                    setTimeout(async function () {
+                        await (await page).click("#btnAds");
+                        console.log(chalk.green('Started ad playback... process will automatically exit in 1 minute'));
+                        resolve();
+                    }, 2000);
+                })
+            }
+            await click();
+            function sleep() {
+                return new Promise(resolve => {
+                    setTimeout(async function () {
+                        console.log(chalk.green('Finished. Check your rewarded ads page at https://politicsandwar.com/rewarded-ads/ to see if you\'ve received your reward'));
+                        process.exit(0);
+                    }, ADLENGTH);
+                })
+            }
+            await sleep();
+        }
+    }
+}
+
+module.exports.d = d;
